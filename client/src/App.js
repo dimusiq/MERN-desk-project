6395990cb85d7f51a6496b0d2e607888ac02b2fd
dimusiq <dumojke@gmail.com>
2022-04-26 21:49:58 +0500

    added client, pages, components and authSlice with Redux

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
+import Header from './components/header.component';
+
+
+import Home from './pages/Home.page';
+import Login from './pages/Login.page';
+import Register from './pages/Register.page';
+
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
